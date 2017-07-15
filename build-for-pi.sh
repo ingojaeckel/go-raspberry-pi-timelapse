4f579eeb40bf08ffbb3144d38389f7e924f3c6b8
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2017-07-15 14:49:50 -0700

    omit symbol table, debug info, dwarf symbol table - shrinks the binary from >2MB to <1MB

--- build-for-pi.sh
+++ build-for-pi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-GOOS=linux GOARCH=arm GOARM=6 CGO_ENABLED=0 go build -v -a -o bin/arm/go-raspberry-pi-timelapse
+GOOS=linux GOARCH=arm GOARM=6 CGO_ENABLED=0 go build -v -a -o bin/arm/go-raspberry-pi-timelapse -ldflags="-s -w"
 file bin/arm/go-raspberry-pi-timelapse
 du bin/arm/go-raspberry-pi-timelapse
