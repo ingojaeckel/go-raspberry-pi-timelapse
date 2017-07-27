3050e029337e42d21d1ad07c1a3ff1c968a8fd76
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2017-07-26 21:07:25 -0700

    fixed typo

--- etc/os/3_install_timelapse_service.sh
+++ etc/os/3_install_timelapse_service.sh
@@ -3,4 +3,5 @@
 cp timelapse_service.sh /etc/init.d/
 chmod 755 /etc/init.d/timelapse_service.sh
 
-ln -fs /etc/rc5.d/S100timelapse /etc/init.d/timelapse_service.sh
+ln -fs /etc/init.d/timelapse_service.sh /etc/rc5.d/S100timelapse
+
