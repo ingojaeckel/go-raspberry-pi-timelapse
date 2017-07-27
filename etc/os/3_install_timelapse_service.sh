5459c9d93f51fab71a132dd63290737eea5932aa
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2017-07-26 21:06:14 -0700

    force link creation

--- etc/os/3_install_timelapse_service.sh
+++ etc/os/3_install_timelapse_service.sh
@@ -2,4 +2,5 @@
 
 cp timelapse_service.sh /etc/init.d/
 chmod 755 /etc/init.d/timelapse_service.sh
-ln -s /etc/rc5.d/S100timelapse /etc/init.d/timelapse_service.sh
+
+ln -fs /etc/rc5.d/S100timelapse /etc/init.d/timelapse_service.sh
