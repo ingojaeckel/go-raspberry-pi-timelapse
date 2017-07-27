5e847ffe065ef1b51a6def59c6f25c78b144da2d
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2017-07-26 21:15:09 -0700

    start timelapse in other runlevels (2,3) as well

--- etc/os/3_install_timelapse_service.sh
+++ etc/os/3_install_timelapse_service.sh
@@ -3,5 +3,7 @@
 cp timelapse_service.sh /etc/init.d/
 chmod 755 /etc/init.d/timelapse_service.sh
 
+ln -fs /etc/init.d/timelapse_service.sh /etc/rc2.d/S100timelapse
+ln -fs /etc/init.d/timelapse_service.sh /etc/rc3.d/S100timelapse
 ln -fs /etc/init.d/timelapse_service.sh /etc/rc5.d/S100timelapse
 
