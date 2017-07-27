2bd44b40fc84f915fbd92ba5256f075c2ba77ea0
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2017-07-26 21:24:55 -0700

    use systemd init

--- etc/os/3_install_timelapse_service.sh
+++ etc/os/3_install_timelapse_service.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-cp timelapse_service.sh /etc/init.d/
-chmod 755 /etc/init.d/timelapse_service.sh
-
-ln -fs /etc/init.d/timelapse_service.sh /etc/rc2.d/S100timelapse
-ln -fs /etc/init.d/timelapse_service.sh /etc/rc3.d/S100timelapse
-ln -fs /etc/init.d/timelapse_service.sh /etc/rc5.d/S100timelapse
+cp timelapse.service /lib/systemd/system/
+chmod 755 /lib/systemd/system/timelapse.service
+systemctl enable timelapse.service
 
