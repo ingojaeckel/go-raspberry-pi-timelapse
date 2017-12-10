e2b4c2d55b271b6657031ead2245b42cfacff029
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2017-12-10 14:23:43 -0800

    added script for initial setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# You might want to download & execute this to start the installation via:
+# wget -O - https://raw.githubusercontent.com/ingojaeckel/go-raspberry-pi-timelapse/master/setup.sh | bash
+#
+# Or use this shorter URL:
+#
+# wget -O - http://bit.ly/2BRqNFZ | bash
+#
+sudo apt-get update -y
+sudo apt-get safe-upgrade -y
+sudo apt-get install -y htop screen git python-smbus i2c-tools
+git clone git@github.com:ingojaeckel/go-raspberry-pi-timelapse.git
+cd go-raspberry-pi-timelapse/
