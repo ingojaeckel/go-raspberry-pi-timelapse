5af15afeede345ba3f760c19004e996d1c82161c
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2019-11-05 20:39:03 +0000

    aptitude is not installed by default

--- etc/os/2_install_timelapse_dependencies.sh
+++ etc/os/2_install_timelapse_dependencies.sh
@@ -3,8 +3,7 @@
 
 TARGET=/home/pi/go-raspberry-pi-timelapse
 
-aptitude update
-aptitude safe-upgrade -y
+apt-get update
 apt-get install -y htop screen git python-smbus i2c-tools
 wget https://github.com/ingojaeckel/go-raspberry-pi-timelapse/raw/master/bin/arm/go-raspberry-pi-timelapse -o ${TARGET}
 chmod 755 ${TARGET}
