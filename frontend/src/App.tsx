5db230f7bccd5cf17ba55328cc3c8306edc778cc
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2020-02-15 16:31:10 +0000

    added centered layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Grid from '@material-ui/core/Grid';
 import HomeIcon from '@material-ui/icons/Home';
 import SettingsIcon from '@material-ui/icons/Settings';
 import TimelineIcon from '@material-ui/icons/Timeline';
-import { List, Avatar, ListItemAvatar, ListItem, ListItemText, Divider } from '@material-ui/core';
+import { Container, List, Avatar, ListItemAvatar, ListItem, ListItemText, Divider, CssBaseline } from '@material-ui/core';
 import Switcher from './Switch';
 
 const customHistory = createBrowserHistory();
@@ -13,40 +13,45 @@ const customHistory = createBrowserHistory();
 function App() {
   return (
     <Router history={customHistory}>
-      <Grid container>
-        <Grid item xs={1}>
-          <List>
-            <ListItem to="/" component={NavLink}>
-              <ListItemAvatar>
-                <Avatar>
-                  <HomeIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Photos" />
-            </ListItem>
-            <ListItem to="/monitoring" component={NavLink}>
-              <ListItemAvatar>
-                <Avatar>
-                  <TimelineIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Monitoring" />
-            </ListItem>
-            <Divider />
-            <ListItem to="/settings" component={NavLink}>
-              <ListItemAvatar>
-                <Avatar>
-                  <SettingsIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Settings" />
-            </ListItem>
-          </List>
+      <React.Fragment>
+        <CssBaseline />
+      <Container fixed>
+        <Grid container>
+          <Grid item xs={2}>
+            <List>
+              <ListItem to="/" component={NavLink}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <HomeIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Photos" />
+              </ListItem>
+              <ListItem to="/monitoring" component={NavLink}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <TimelineIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Monitoring" />
+              </ListItem>
+              <Divider />
+              <ListItem to="/settings" component={NavLink}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <SettingsIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Settings" />
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={10}>
+            <Switcher />
+          </Grid>
         </Grid>
-        <Grid item xs={11}>
-          <Switcher />
-        </Grid>
-      </Grid>
+      </Container>
+      </React.Fragment>
     </Router>
   );
 }
