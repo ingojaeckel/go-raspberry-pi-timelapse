3311a613e8b7766b0f42be18404131acf00ebed6
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2021-02-24 21:03:21 +0000

    fix remaining compilation errors

--- frontend/src/components/PhotoListComponent.tsx
+++ frontend/src/components/PhotoListComponent.tsx
@@ -19,43 +19,9 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
 import Axios from 'axios';
 import { BaseUrl } from '../conf/config';
-import { PhotosResponse } from '../models/response';
+import { PhotosResponse, Photo } from '../models/response';
 
 // This is adapted from https://material-ui.com/components/tables/
-
-interface Data {
-    calories: number;
-    carbs: number;
-    fat: number;
-    name: string;
-    protein: number;
-}
-
-function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
-): Data {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Donut', 452, 25.0, 51, 4.9),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Honeycomb', 408, 3.2, 87, 6.5),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Jelly Bean', 375, 0.0, 94, 0.0),
-    createData('KitKat', 518, 26.0, 65, 7.0),
-    createData('Lollipop', 392, 0.2, 98, 0.0),
-    createData('Marshmallow', 318, 0, 81, 2.0),
-    createData('Nougat', 360, 19.0, 9, 37.0),
-    createData('Oreo', 437, 18.0, 63, 4.0),
-];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
     if (b[orderBy] < a[orderBy]) {
@@ -90,21 +56,21 @@ function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
 
 interface HeadCell {
     disablePadding: boolean;
-    id: keyof Data;
+    id: keyof Photo;
     label: string;
     numeric: boolean;
 }
 
 const headCells: HeadCell[] = [
-    { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
-    { id: 'calories', numeric: true, disablePadding: false, label: 'Date' },
-    { id: 'fat', numeric: true, disablePadding: false, label: 'Size' },
+    { id: 'Name', numeric: false, disablePadding: true, label: 'Name' },
+    { id: 'ModTime', numeric: false, disablePadding: false, label: 'Date' },
+    { id: 'Size', numeric: true, disablePadding: false, label: 'Size' },
 ];
 
 interface EnhancedTableProps {
     classes: ReturnType<typeof useStyles>;
     numSelected: number;
-    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Photo) => void;
     onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => void;
     order: Order;
     orderBy: string;
@@ -113,7 +79,7 @@ interface EnhancedTableProps {
 
 function EnhancedTableHead(props: EnhancedTableProps) {
     const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
-    const createSortHandler = (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+    const createSortHandler = (property: keyof Photo) => (event: React.MouseEvent<unknown>) => {
         onRequestSort(event, property);
     };
 
@@ -190,28 +156,11 @@ const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
                 [classes.highlight]: numSelected > 0,
             })}
         >
-            {numSelected > 0 ? (
-                <Typography className={classes.title} color="inherit" variant="subtitle1">
-                    {numSelected} selected
-        </Typography>
-            ) : (
-                    <Typography className={classes.title} variant="h6" id="tableTitle">
-                        Photos
-        </Typography>
-                )}
-            {numSelected > 0 ? (
-                <Tooltip title="Delete">
-                    <IconButton aria-label="delete">
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
-            ) : (
-                    <Tooltip title="Filter list">
-                        <IconButton aria-label="filter list">
-                            <FilterListIcon />
-                        </IconButton>
-                    </Tooltip>
-                )}
+            <Tooltip title="Delete">
+                <IconButton aria-label="delete">
+                    <DeleteIcon />
+                </IconButton>
+            </Tooltip>
         </Toolbar>
     );
 };
@@ -245,7 +194,7 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function PhotoListComponent() {
     const classes = useStyles();
     const [order, setOrder] = React.useState<Order>('asc');
-    const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');
+    const [orderBy, setOrderBy] = React.useState<keyof Photo>('ModTime');
     const [selected, setSelected] = React.useState<string[]>([]);
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
@@ -257,21 +206,6 @@ export default function PhotoListComponent() {
                 console.log(resp.data)
             })
     }, []);
-
-    const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof Data) => {
-        const isAsc = orderBy === property && order === 'asc';
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(property);
-    };
-
-    const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.checked) {
-            const newSelecteds = rows.map(n => n.name);
-            setSelected(newSelecteds);
-            return;
-        }
-        setSelected([]);
-    };
 
     const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
         const selectedIndex = selected.indexOf(name);
@@ -304,6 +238,16 @@ export default function PhotoListComponent() {
 
     const isSelected = (name: string) => selected.indexOf(name) !== -1;
 
+    const rows = [
+        {Name: "pic1", ModTime: 1, Size: 1 },
+        {Name: "pic2", ModTime: 2, Size: 1 },
+        {Name: "pic3", ModTime: 3, Size: 1 },
+    ];
+
+    const handleSelectAllClick = () => {};
+    
+    const handleRequestSort = () => {};
+
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
     return (
@@ -330,17 +274,17 @@ export default function PhotoListComponent() {
                             {stableSort(rows, getComparator(order, orderBy))
                                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                                 .map((row, index) => {
-                                    const isItemSelected = isSelected(row.name);
+                                    const isItemSelected = isSelected(row.Name);
                                     const labelId = `enhanced-table-checkbox-${index}`;
 
                                     return (
                                         <TableRow
                                             hover
-                                            onClick={event => handleClick(event, row.name)}
+                                            onClick={event => handleClick(event, row.Name)}
                                             role="checkbox"
                                             aria-checked={isItemSelected}
                                             tabIndex={-1}
-                                            key={row.name}
+                                            key={row.Name}
                                             selected={isItemSelected}
                                         >
                                             <TableCell padding="checkbox">
@@ -350,10 +294,10 @@ export default function PhotoListComponent() {
                                                 />
                                             </TableCell>
                                             <TableCell component="th" id={labelId} scope="row" padding="none">
-                                                {row.name}
+                                                {row.Name}
                                             </TableCell>
-                                            <TableCell align="right">{row.calories}</TableCell>
-                                            <TableCell align="right">{row.fat}</TableCell>
+                                            <TableCell align="right">{row.ModTime}</TableCell>
+                                            <TableCell align="right">{row.Size}</TableCell>
                                         </TableRow>
                                     );
                                 })}
