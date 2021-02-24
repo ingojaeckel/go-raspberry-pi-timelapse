8fd11f5c74c96537b1cca98fa3e56b0eb48700c9
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2021-02-24 21:14:59 +0000

    integrate data grid to replace table

--- frontend/src/components/PhotoComponent.tsx
+++ frontend/src/components/PhotoComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PhotoListComponent from './PhotoListComponent';
+import PhotoListDataGridComponent from './PhotoListDataGridComponent';
 import { Typography, Button } from '@material-ui/core';
 
 export default function PhotosComponent() {
@@ -8,7 +8,7 @@ export default function PhotosComponent() {
       <Typography variant="h5" component="h5">Preview</Typography>
       <Typography variant="body1">Current photo goes here.</Typography>
       <Button variant="contained">Download</Button>
-      <PhotoListComponent />
+      <PhotoListDataGridComponent />
     </React.Fragment>
   );
 }
