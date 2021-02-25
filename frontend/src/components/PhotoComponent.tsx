6ff7c96861e94f70ace18e469c5c3919bb2d0fd4
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2021-02-25 16:39:26 +0000

    basic selection of photos to prepare for delete/download operations
    connected /capture endpoint for photo preview
    groups buttons (refresh, download, delete), unified button UI betw/ photo and setup screens

--- frontend/src/components/PhotoComponent.tsx
+++ frontend/src/components/PhotoComponent.tsx
@@ -1,14 +1,90 @@
-import React from 'react';
-import PhotoListDataGridComponent from './PhotoListDataGridComponent';
-import { Typography, Button } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Typography, Button, ButtonGroup } from '@material-ui/core';
+import axios from 'axios';
+import { DataGrid, ColDef, RowData, RowId, SelectionModelChangeParams } from '@material-ui/data-grid';
+import { PhotosResponse } from '../models/response';
+import { BaseUrl } from '../conf/config'
+
+export interface PhotosRowData {
+  Photos: RowData[],
+  Selected: RowId[],
+}
+
+const columns: ColDef[] = [
+  { field: 'fileName', headerName: 'Name', width: 300 },
+  { field: 'fileCreateTime', headerName: 'Created At', width: 300 },
+  { field: 'fileSizeBytes', headerName: 'Size', width: 100 },
+];
 
 export default function PhotosComponent() {
+  const [state, setState] = useState<PhotosRowData>({
+    Photos: [],
+    Selected: [],
+  });
+
+const getPhotos = () => {
+  axios
+    .get<PhotosResponse>(BaseUrl + "/photos")
+    .then(resp => {
+      // After receiving a response, map the PhotosResponse to RowData[] which can be displayed in the data grid.
+      if (resp.data) {
+        var rows: RowData[] = [];
+
+        for (var i=0; i<resp.data.Photos.length; i++) {
+          let photo = resp.data.Photos[i];
+          rows.push({
+            id: photo.Name,
+            isSelected: false,
+            fileName: photo.Name,
+            fileCreateTime: photo.ModTime,
+            fileSizeBytes: photo.Size})
+        }
+
+        setState({
+          Photos: rows,
+          Selected: []});
+      }
+    });
+  }
+
+  useEffect(() => {
+    getPhotos()
+  }, []);
+
+  const handleSelectionModelChanged = (selectionModel: SelectionModelChangeParams) => {
+    console.log("selection changed: ", selectionModel);
+
+    setState({
+      Photos: state.Photos,
+      Selected: selectionModel.selectionModel});
+  };
+
+  const handleRefreshClicked = () => {
+    console.log("refresh clicked");
+    getPhotos();
+  };
+  const handleDownloadClicked = () => { console.log("download clicked: ", state.Selected) };
+  const handleDeleteClicked = () => { console.log("delete clicked: ", state.Selected) };
+
   return (
     <React.Fragment>
       <Typography variant="h5" component="h5">Preview</Typography>
-      <Typography variant="body1">Current photo goes here.</Typography>
-      <Button variant="contained">Download</Button>
-      <PhotoListDataGridComponent />
-    </React.Fragment>
+      <Typography variant="body1">
+        <img src={BaseUrl+"/capture"} alt="preview" />
+      </Typography>
+      <ButtonGroup color="primary" aria-label="outlined primary button group">
+        <Button onClick={handleRefreshClicked}>Refresh</Button>
+        <Button onClick={handleDownloadClicked}>Download</Button>
+        <Button onClick={handleDeleteClicked}>Delete</Button>  
+      </ButtonGroup>
+      <div style={{ height: 500, width: '100%' }}>
+        <DataGrid
+          rows={state.Photos}
+          columns={columns}
+          checkboxSelection={true}
+          disableSelectionOnClick={true}
+          onSelectionModelChange={handleSelectionModelChanged} />
+        </div>
+      </React.Fragment>
   );
 }
