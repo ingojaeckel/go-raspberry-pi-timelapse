c194366603976087e2a9ee7056cce9b22100988c
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2021-02-26 13:34:08 +0000

    fix regression in pi/arm build. include arm in ci/cd

--- build-for-pi.sh
+++ build-for-pi.sh
@@ -5,10 +5,10 @@ go test -v -cover ./...
 
 GIT_COMMIT=`git rev-parse --short HEAD`
 BUILT_AT=`date`
-GOOS=linux
-GOARCH=arm
-GOARM=6
-CGO_ENABLED=0
+GOOS=linux \
+GOARCH=arm \
+GOARM=6 \
+CGO_ENABLED=0 \
 go build -v -a -o bin/arm/go-raspberry-pi-timelapse -ldflags="-s -w -X 'main.gitCommit=${GIT_COMMIT}' -X 'main.builtAt=${BUILT_AT}'"
 file bin/arm/go-raspberry-pi-timelapse
 du bin/arm/go-raspberry-pi-timelapse
