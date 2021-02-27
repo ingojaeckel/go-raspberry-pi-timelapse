f03fedf0df10e8a17b6393f5ef0d01a5b31be744
Ingo <ingo.jaeckel@googlemail.com>
2021-02-27 23:41:38 +0000

    Add CLI flag support for 4x initial flags (#8)
    * add CLI flag support for 4x initial flags
    
    * moved flag/conf code into conf package
    
    * cleanup
    
    Co-authored-by: Ingo Jaeckel <ingo.jaeckel@gmail.com>

--- conf/conf.go
+++ conf/conf.go
@@ -1,13 +1,32 @@
 package conf
 
 const (
-	ListenAddress            = ":8080"
+	DefaultListenAddress     = ":8080"
+	DefaultLogToFile         = false
 	MaxFileSizeBytes         = 100485760 // 100 MB
 	HeaderContentType        = "Content-Type"
 	HeaderContentDisposition = "Content-Disposition"
 	HeaderContentTypeJSON    = "application/json"
-	StorageFolder            = "timelapse-pictures"
+	DefaultStorageFolder     = "timelapse-pictures"
 	TempFilesFolder          = "/tmp"
 	LogFile                  = "timelapse.log"
-	LogToFile                = false
 )
+
+var (
+	LogToFile     = DefaultLogToFile
+	ListenAddress = DefaultListenAddress
+	StorageFolder = DefaultStorageFolder
+)
+
+// Update Updates configuration depending on the state of CLI flags provided.
+func Update(listenAddress *string, storageAddress *string, logToFile *bool) {
+	if logToFile != nil {
+		LogToFile = *logToFile
+	}
+	if listenAddress != nil {
+		ListenAddress = *listenAddress
+	}
+	if storageAddress != nil {
+		StorageFolder = *storageAddress
+	}
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -24,21 +25,32 @@ var content embed.FS
 
 func main() {
 	initVersion()
+
+	versionFlag := flag.Bool("version", false, "Print version and exit.")
+	listenAddress := flag.String("port", conf.DefaultListenAddress, "HTTP port to listen on.")
+	logToFile := flag.Bool("logToFile", conf.DefaultLogToFile, "Toggle to enable logging to a file on disk instead of stdout. Logging to a file is recommended for long term operation.")
+	storageAddress := flag.String("storageFolder", conf.DefaultStorageFolder, "Folder for storage of timelapse pictures.")
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Println(version)
+		return
+	}
+	conf.Update(listenAddress, storageAddress, logToFile)
 	if err := initLogging(); err != nil {
 		log.Fatalf("Failed to initialize logging. Unable to start. Cause: %s", err.Error())
 		return
 	}
+
 	s, err := conf.LoadConfiguration()
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %s", err.Error())
 		return
 	}
-
-	log.Println("Version: ", version)
 	log.Printf("Seconds between captures: %d\n", s.SecondsBetweenCaptures)
 	log.Printf("Offset within hour:       %d\n", s.OffsetWithinHour)
 	log.Printf("Resolution:               %d x %d\n", s.PhotoResolutionWidth, s.PhotoResolutionHeight)
-	log.Printf("Listening on port:        %s...\n", conf.ListenAddress)
+	log.Printf("Listen address:           %s\n", conf.ListenAddress)
 
 	mux := goji.NewMux()
 
@@ -72,6 +84,7 @@ func main() {
 		t.CapturePeriodically()
 	}
 
+	log.Println("Listening...")
 	if err := http.ListenAndServe(conf.ListenAddress, mux); err != nil {
 		log.Fatal("Failed start: ", err.Error())
 	}
@@ -85,7 +98,7 @@ func initLogging() error {
 		}
 		log.SetOutput(f)
 	}
-	log.Printf("Started at %s\n", time.Now())
+	log.Printf("Version %s started at %s\n", version, time.Now())
 	return nil
 }
 
