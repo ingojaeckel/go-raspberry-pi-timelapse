f0b388a94e7ef46c0ccb7ebf6e66e00a4e3a9eed
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2021-03-13 22:06:57 +0000

    removed ListItemText as it is repeated in title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Grid from '@material-ui/core/Grid';
 import HomeIcon from '@material-ui/icons/Home';
 import SettingsIcon from '@material-ui/icons/Settings';
 import TimelineIcon from '@material-ui/icons/Timeline';
-import { Container, List, Avatar, ListItemAvatar, ListItem, ListItemText, Divider, CssBaseline } from '@material-ui/core';
+import { Container, List, Avatar, ListItemAvatar, ListItem, CssBaseline } from '@material-ui/core';
 import Switcher from './Switch';
 import axios from 'axios';
 
@@ -24,7 +24,7 @@ function App() {
         <CssBaseline />
         <Container fixed>
           <Grid container spacing={2}>
-            <Grid item xs={2}>
+            <Grid item xs={1}>
               <List>
                 <ListItem to={`${process.env.PUBLIC_URL}/`} component={NavLink}>
                   <ListItemAvatar>
@@ -32,7 +32,6 @@ function App() {
                       <HomeIcon />
                     </Avatar>
                   </ListItemAvatar>
-                  <ListItemText primary="Photos" />
                 </ListItem>
                 <ListItem to={`${process.env.PUBLIC_URL}/monitoring`} component={NavLink}>
                   <ListItemAvatar>
@@ -40,16 +39,13 @@ function App() {
                       <TimelineIcon />
                     </Avatar>
                   </ListItemAvatar>
-                  <ListItemText primary="Monitoring" />
                 </ListItem>
-                <Divider />
                 <ListItem to={`${process.env.PUBLIC_URL}/settings`} component={NavLink}>
                   <ListItemAvatar>
                     <Avatar>
                       <SettingsIcon />
                     </Avatar>
                   </ListItemAvatar>
-                  <ListItemText primary="Settings" />
                 </ListItem>
               </List>
             </Grid>
