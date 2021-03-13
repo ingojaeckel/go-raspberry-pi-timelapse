89403ca3905f23a50e0b1f5b58769eae836ce9e2
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2021-03-13 19:55:30 +0000

    cleanup, added note about current issue

--- frontend/src/components/SetupComponent.tsx
+++ frontend/src/components/SetupComponent.tsx
@@ -31,18 +31,12 @@ export default function SetupComponent() {
 
   const handleRestartClicked = () => {
     console.log("restart clicked");
-
-    axios
-      .get(BaseUrl + "/admin/restart")
-      .then(() => console.log("restart initiated"));
+    axios.get(BaseUrl + "/admin/restart").then(() => console.log("restart initiated"));
   };
 
   const handleShutdownClicked = () => {
     console.log("shutdown clicked");
-
-    axios
-      .get(BaseUrl + "/admin/shutdown")
-      .then(() => console.log("shutdown initiated"));
+    axios.get(BaseUrl + "/admin/shutdown").then(() => console.log("shutdown initiated"));
   };
 
   function handleSaveSettingsClicked() {
@@ -54,36 +48,27 @@ export default function SetupComponent() {
       });
   };
 
-  function handleOffsetChanged(_event: ChangeEvent<{}>, value: number | number[]) {
-    setState(Object.assign(state, { OffsetWithinHour: value as number }))
-  }
-  function handleQualityChanged(_event: ChangeEvent<{}>, value: number | number[]) {
-    setState(Object.assign(state, { Quality: value as number }));
-  }
-  function handleRotationChanged(_event: ChangeEvent<{}>, value: number | number[]) {
-    setState(Object.assign(state, { RotateBy: value as number }));
-  }
-  function handleTimeBetweenCapturesChanged(event: ChangeEvent<{}>, value: number | number[]) {
-    const newVal = (value as number) * 60;
-    const newState = Object.assign(state, { SecondsBetweenCaptures: newVal })
-    setState(newState);
-  }
+  const handleOffsetChanged = (_event: ChangeEvent<{}>, value: number | number[]) => setState(Object.assign(state, { OffsetWithinHour: value as number }));
+  const handleQualityChanged = (_event: ChangeEvent<{}>, value: number | number[]) => setState(Object.assign(state, { Quality: value as number }));
+  const handleRotationChanged = (_event: ChangeEvent<{}>, value: number | number[]) => setState(Object.assign(state, { RotateBy: value as number }));
+  const handleTimeBetweenCapturesChanged = (_event: ChangeEvent<{}>, value: number | number[]) => setState(Object.assign(state, { SecondsBetweenCaptures: (value as number) * 60 }));
 
+  // TODO Fix bug: Values only update on save
   return (
     <React.Fragment>
       <div>
         <Typography gutterBottom>Time between captures (minutes): {state.SecondsBetweenCaptures/60}</Typography>
-        <Slider valueLabelDisplay="auto" defaultValue={0} step={1} marks min={1} max={30} onChange={handleTimeBetweenCapturesChanged} />
+        <Slider valueLabelDisplay="auto" step={1} marks min={1} max={30} value={state.SecondsBetweenCaptures/60} onChange={handleTimeBetweenCapturesChanged} />
         <Typography gutterBottom>Delay within value hour before first capture (minutes): {state.OffsetWithinHour}</Typography>
-        <Slider valueLabelDisplay="auto" defaultValue={0} step={5} marks min={0} max={30} onChange={handleOffsetChanged} />
+        <Slider valueLabelDisplay="auto" step={5} marks min={0} max={30} value={state.OffsetWithinHour} onChange={handleOffsetChanged} />
         <Typography gutterBottom>Photo Resolution (pixels)</Typography>
         <Select defaultValue={0}>
           <MenuItem value={0}>3280x2464</MenuItem>
         </Select>
         <Typography gutterBottom>Rotation (degrees): {state.RotateBy}</Typography>
-        <Slider valueLabelDisplay="auto" defaultValue={0} step={180} marks min={0} max={180} onChange={handleRotationChanged} />
+        <Slider valueLabelDisplay="auto" step={180} marks min={0} max={180} value={state.RotateBy} onChange={handleRotationChanged} />
         <Typography gutterBottom>Photo Quality (0..100%): {state.Quality}</Typography>
-        <Slider valueLabelDisplay="auto" defaultValue={0} step={5} marks min={0} max={100} onChange={handleQualityChanged} />
+        <Slider valueLabelDisplay="auto" step={5} marks min={0} max={100} value={state.Quality} onChange={handleQualityChanged} />
       </div>
       <ButtonGroup color="primary" aria-label="outlined primary button group">
         <Button onClick={handleSaveSettingsClicked}>Save</Button>
