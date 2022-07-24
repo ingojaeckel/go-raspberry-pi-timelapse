7bcd6743f07e5c99175fd7267f02a32fd8f61e9f
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2022-07-24 16:56:21 +0100

    Ensure settings values can be iterated on before save

--- frontend/src/components/SetupComponent.tsx
+++ frontend/src/components/SetupComponent.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, ChangeEvent } from 'react';
 import axios from 'axios';
 import { BaseUrl } from '../conf/config'
 import { SettingsResponse } from '../models/response'
-import { ButtonGroup, Button, Select, MenuItem, Slider, Typography } from '@material-ui/core';
+import { ButtonGroup, Button, Select, MenuItem, Typography, TextField, Grid } from '@material-ui/core';
 
 export default function SetupComponent() {
   const [state, setState] = useState<SettingsResponse>({
@@ -29,6 +29,8 @@ export default function SetupComponent() {
       });
   }, []);
 
+
+
   const handleRestartClicked = () => {
     console.log("restart clicked");
     axios.get(BaseUrl + "/admin/restart").then(() => console.log("restart initiated"));
@@ -48,10 +50,10 @@ export default function SetupComponent() {
       });
   };
 
-  const handleOffsetChanged = (_event: ChangeEvent<{}>, value: number | number[]) => setState(Object.assign(state, { OffsetWithinHour: value as number }));
-  const handleQualityChanged = (_event: ChangeEvent<{}>, value: number | number[]) => setState(Object.assign(state, { Quality: value as number }));
-  const handleRotationChanged = (_event: ChangeEvent<{}>, value: number | number[]) => setState(Object.assign(state, { RotateBy: value as number }));
-  const handleTimeBetweenCapturesChanged = (_event: ChangeEvent<{}>, value: number | number[]) => setState(Object.assign(state, { SecondsBetweenCaptures: (value as number) * 60 }));
+  const handleOffsetChanged = (e: ChangeEvent<HTMLInputElement>) => setState(Object.assign(state, { OffsetWithinHour: parseInt(e.target.value) as number }));
+  const handleQualityChanged = (e: ChangeEvent<HTMLInputElement>) => setState(Object.assign(state, { Quality: parseInt(e.target.value) as number }));
+  const handleRotationChanged = (e: ChangeEvent<HTMLInputElement>) => setState(Object.assign(state, { RotateBy: parseInt(e.target.value) as number }));
+  const handleTimeBetweenCapturesChanged = (e: ChangeEvent<HTMLInputElement>) => setState(Object.assign(state, { SecondsBetweenCaptures: (parseInt(e.target.value) as number) * 60 }));
 
   // TODO Fix bug: Values only update on save
   return (
@@ -59,20 +61,42 @@ export default function SetupComponent() {
       <div>
         <Typography variant="h4" component="h4">Settings</Typography>
       </div>
-      <div>
-        <Typography gutterBottom>Time between captures (minutes): {state.SecondsBetweenCaptures/60}</Typography>
-        <Slider valueLabelDisplay="auto" step={1} marks min={1} max={30} value={state.SecondsBetweenCaptures/60} onChange={handleTimeBetweenCapturesChanged} />
-        <Typography gutterBottom>Delay within value hour before first capture (minutes): {state.OffsetWithinHour}</Typography>
-        <Slider valueLabelDisplay="auto" step={5} marks min={0} max={30} value={state.OffsetWithinHour} onChange={handleOffsetChanged} />
-        <Typography gutterBottom>Photo Resolution (pixels)</Typography>
-        <Select defaultValue={0}>
-          <MenuItem value={0}>3280x2464</MenuItem>
-        </Select>
-        <Typography gutterBottom>Rotation (degrees): {state.RotateBy}</Typography>
-        <Slider valueLabelDisplay="auto" step={180} marks min={0} max={180} value={state.RotateBy} onChange={handleRotationChanged} />
-        <Typography gutterBottom>Photo Quality (0..100%): {state.Quality}</Typography>
-        <Slider valueLabelDisplay="auto" step={5} marks min={0} max={100} value={state.Quality} onChange={handleQualityChanged} />
-      </div>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography gutterBottom>Time between captures (minutes):</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField type="number" onChange={handleTimeBetweenCapturesChanged} defaultValue={state.SecondsBetweenCaptures/60} inputProps={{ inputMode: 'numeric', pattern: '[0-9]+' }}  />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography gutterBottom>Delay within value hour before first capture (minutes): {state.OffsetWithinHour}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField type="number" defaultValue={state.OffsetWithinHour} onChange={handleOffsetChanged} />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography gutterBottom>Photo Resolution (pixels)</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Select defaultValue={0}>
+            <MenuItem value={0}>3280x2464</MenuItem>
+          </Select>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography gutterBottom>Rotation (degrees): {state.RotateBy}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField type="number" defaultValue={state.RotateBy} onChange={handleRotationChanged} />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography gutterBottom>Photo Quality (0..100%): {state.Quality}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField type="number" defaultValue={state.Quality} onChange={handleQualityChanged} />
+        </Grid>
+        <Grid item xs={6}>
+        </Grid>
+      </Grid>
       <ButtonGroup color="primary" aria-label="outlined primary button group">
         <Button onClick={handleSaveSettingsClicked}>Save</Button>
         <Button onClick={handleRestartClicked}>Restart</Button>
