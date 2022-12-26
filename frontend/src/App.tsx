222c95bcfc7972ec75c6172cec84d37ef14bb5a1
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2022-12-26 20:43:07 +0000

    replace href based nav w/ single page app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { createBrowserHistory } from "history";
-import { NavLink, Router } from 'react-router-dom';
-import Switcher from './Switch';
 import axios from 'axios';
-import { Avatar, Container, CssBaseline, Grid, List, ListItem, ListItemAvatar, Tab, Tabs } from '@mui/material';
+import { Box, Container, CssBaseline, Grid, Tab, Tabs, Typography } from '@mui/material';
 import { Home, PhotoCamera, Timeline, Settings, Description } from '@mui/icons-material'
+import PhotoComponent from './components/PhotoComponent';
+import MonitoringComponent from './components/MonitoringComponent';
+import SetupComponent from './components/SetupComponent';
+import PreviewComponent from './components/PreviewComponent';
+import LogComponent from './components/LogComponent';
 
 // Register default request interceptors
 axios.interceptors.request.use(request => {
@@ -12,56 +14,72 @@ axios.interceptors.request.use(request => {
   return request
 })
 
-const customHistory = createBrowserHistory();
-
-interface LinkTabProps {
-  label: string;
-  href: string;
-  icon: React.ReactElement;
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
-function LinkTab(props: LinkTabProps) {
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <Tab
-      component="a"
-      onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        //event.preventDefault(); // TODO currently needs to be disabled to enable navigation
-      }}
-      {...props}
-    />
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
   );
 }
 
 function App() {
   const [value, setValue] = React.useState(0);
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
   return (
-    <Router history={customHistory}>
-      <React.Fragment>
-        <CssBaseline />
-        <Container fixed>
-          <Grid container spacing={0}>
-            <Grid item xs={12}>
-              <Tabs value={value} onChange={handleChange} variant="scrollable" scrollButtons allowScrollButtonsMobile aria-label="scrollable icon label tabs">
-                <LinkTab label="home" href={`${process.env.PUBLIC_URL}/`} icon={<Home />} />
-                <LinkTab label="preview" href={`${process.env.PUBLIC_URL}/preview`} icon={<PhotoCamera />} />
-                <LinkTab label="monitoring" href={`${process.env.PUBLIC_URL}/monitoring`} icon={<Timeline />} />
-                <LinkTab label="settings" href={`${process.env.PUBLIC_URL}/settings`} icon={<Settings />} />
-                <LinkTab label="logs" href={`${process.env.PUBLIC_URL}/logs`} icon={<Description />} />
-              </Tabs>
-            </Grid>
-            <Grid item xs={12}>
-              <Switcher />
-            </Grid>
-            <Grid item xs={12}>
-              <div className="footer">version: <a href={"https://github.com/ingojaeckel/go-raspberry-pi-timelapse/commit/" + process.env.REACT_APP_GIT_SHA}>{process.env.REACT_APP_GIT_SHA}</a></div>
-            </Grid>
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Grid container spacing={0}>
+          <Grid item xs={12}>
+            <Tabs value={value} onChange={handleChange} variant="scrollable" scrollButtons allowScrollButtonsMobile aria-label="scrollable icon label tabs">
+              <Tab label="home" icon={<Home />} />
+              <Tab label="preview" icon={<PhotoCamera />} />
+              <Tab label="monitoring"  icon={<Timeline />} />
+              <Tab label="settings"  icon={<Settings />} />
+              <Tab label="logs"  icon={<Description />} />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+              <PhotoComponent />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <PreviewComponent />
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              <MonitoringComponent />
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+              <SetupComponent />
+            </TabPanel>
+            <TabPanel value={value} index={4}>
+              <LogComponent />
+            </TabPanel>
           </Grid>
-        </Container>
-      </React.Fragment>
-    </Router>
+          <Grid item xs={12}>
+            <div className="footer">version: <a href={"https://github.com/ingojaeckel/go-raspberry-pi-timelapse/commit/" + process.env.REACT_APP_GIT_SHA}>{process.env.REACT_APP_GIT_SHA}</a></div>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
 
